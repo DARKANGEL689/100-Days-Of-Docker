66c3ffc6f74b13170d2d782e36c2de2a573e24c4
DARKANGEL689 <ahadsd1959083@gmail.com>
2026-04-23 20:28:31 +0530

    Day 37 (5/12): Authored Node.js application natively instrumented with prom-client for telemetry exposure

---
+++ Day-37-Observability/monitored-api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const client = require('prom-client');
+const app = express();
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status']
+});
+register.registerMetric(httpRequestCounter);
+
+app.get('/', (req, res) => {
+  httpRequestCounter.inc({ method: 'GET', route: '/', status: 200 });
+  res.send('Hello! I am being monitored by Prometheus.');
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+app.listen(8080, () => console.log('Monitored API running on port 8080'));
